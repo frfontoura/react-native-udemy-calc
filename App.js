ce88a0c92393920cc81e11e7d9da1fdd161ff700
frfontoura <frfontoura@gmail.com>
2019-05-09 20:08:10 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+
+import Button from "./src/components/Button";
+import Display from "./src/components/Display";
+
+export default function App() {
+  const [ displayValue, setDisplayValue] = useState('543'); 
+
+  function addDigit(n) {
+    setDisplayValue(n);
+  }
+
+  function clearMemory() {
+    setDisplayValue('0');
+  }
+
+  function setOperation(operation) {
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <Display value={displayValue} />
+      <View style={styles.buttons}>
+        <Button label="AC" triple onClick={clearMemory} />
+        <Button label="/" operation onClick={setOperation} />
+        <Button label="7" onClick={addDigit} />
+        <Button label="8" onClick={addDigit} />
+        <Button label="9" onClick={addDigit} />
+        <Button label="*" operation onClick={setOperation}/>
+        <Button label="4" onClick={addDigit} />
+        <Button label="5" onClick={addDigit} />
+        <Button label="6" onClick={addDigit} />
+        <Button label="-" operation onClick={setOperation}/>
+        <Button label="1" onClick={addDigit} />
+        <Button label="2" onClick={addDigit} />
+        <Button label="3" onClick={addDigit} />
+        <Button label="+" operation onClick={setOperation} />
+        <Button label="0" double onClick={addDigit} />
+        <Button label="." onClick={addDigit} />
+        <Button label="=" operation onClick={setOperation} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  buttons: {
+    flexDirection: "row",
+    flexWrap: "wrap"
+  }
+});
---
+++ src/components/Button.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, Dimensions, TouchableHighlight } from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    fontSize: 40,
+    height: Dimensions.get('window').width / 4,
+    width: Dimensions.get('window').width / 4,
+    padding: 20,
+    backgroundColor: '#f0f0f0',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#888',
+  },
+  operationButton: {
+    color: '#fff',
+    backgroundColor: '#fa8231'
+  },
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+  buttonTriple: {
+    width: (Dimensions.get('window').width / 4) * 3,
+  }
+})
+
+export default function Button({ label, onClick, double, triple, operation }) {
+  const stylesButton = [ styles.button ];
+  if( double ) stylesButton.push(styles.buttonDouble);
+  if( triple ) stylesButton.push(styles.buttonTriple);
+  if( operation ) stylesButton.push(styles.operationButton);
+
+  return (
+    <TouchableHighlight onPress={() => onClick(label)}>
+      <Text style={stylesButton}>{label}</Text>
+    </TouchableHighlight>
+  );
+}
